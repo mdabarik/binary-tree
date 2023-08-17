bab02d77eeb96033249b05fee97b2d24e2b04243
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-08-17 21:52:31 +0600

    Update 12--populating-next-right-pointers-in-each-node.java

--- 12--populating-next-right-pointers-in-each-node.java
+++ 12--populating-next-right-pointers-in-each-node.java
@@ -1,1 +1,44 @@
-// Populating Next Right Pointers in Each Node
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Node levelStart = root;
+        while (levelStart != null) {
+            Node curr = levelStart;
+            while (curr != null) {
+                if (curr.left != null) {
+                    curr.left.next = curr.right;
+                    if (curr.next != null) {
+                        curr.right.next = curr.next.left;
+                    }
+                }
+                curr = curr.next;
+            }
+            levelStart = levelStart.left;
+        }
+        return root;
+    }
+}
+// TC: O(n), SC: O(1)
